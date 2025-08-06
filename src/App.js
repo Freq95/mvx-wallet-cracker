86d10c71c04dce09b13b84e84cdad358596112a4
Paul Novac <Freq95@users.noreply.github.com>
2025-08-06 04:31:17 +0300

    T0002 - website version added
    - App.js from now on should update the webpage version

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import GuardianSim from './GuardianSim';
+import React from "react";
+import GuardianSim from "./GuardianSim";
 
 function App() {
   return (
-    <div>
-      <GuardianSim />
+    <div className="h-screen flex flex-col bg-gray-900 text-white">
+      <div className="flex-grow overflow-hidden p-4">
+        <GuardianSim />
+      </div>
+      <footer className="text-center text-sm text-gray-400 p-2">
+        v0.1.0.2025
+      </footer>
     </div>
   );
 }
